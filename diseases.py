995331b1934fb317ddd7ef4b6b83c4f6e61fc73b
ivanezeigbo <ivyezeigbo@yahoo.com>
2017-01-08 10:10:38 -0300

    Disease Propagation (Debugged)
    Modeling Disease Propagation during distribution of relief using:
    
    An optimal Hamiltonian circuit that readjusts when transportation between two countries is banned.
    Differential equations modeling spread of disease within a country.
    Simple Algebra modeling spread of diseases between countries dependent on frequency of traveling between those countries (frequency of traveling is under the assumption that the cheapest edges to transverse have the highest frequency of traveling).
    Banning of transportation/elimination of edges when a country has a proportion of infected individuals over the threshold of the neighboring county (each country has his own threshold).
    Relief Distribution stops when it is impossible to transverse a Hamiltonian circuit (that is the banning of transportation/elimination of edges in the network has made it impossible to find a Hamiltonian circuits for the nodes in that current connected graph - completely isolated nodes are ignored when finding a Ham

--- diseases.py
+++ diseases.py
@@ -3,28 +3,26 @@ import matplotlib.pyplot as plt
 from random import *
 
 Cl= {} #country label
-Cl[0] = ['Germany', 0, 12050, 0.8] #[Name of country, amount infected, population size, threshold] in that order
-Cl[1] = ['Belgium', 0, 14000, 0.78] 
-Cl[2] = ['Czech Republic', 0, 9200, 0.9]
-Cl[3] = ['Finland', 0, 1502, 0.76]
-Cl[4] = ['France', 1, 10312, 0.84]
+Cl[0] = ['Germany', 0, 12050, 0.5] #country, infected people, population, threshold between countries
+Cl[1] = ['Belgium', 0, 14000, 0.58] 
+Cl[2] = ['Czech Republic', 0, 9200, 0.7]
+Cl[3] = ['Finland', 0, 1502, 0.46]
+Cl[4] = ['France', 1, 10312, 0.34]
 Cl[5] = ['Estonia', 0, 14005, 0.79]
 Cl[6] = ['Latvia', 0, 13211, 0.67]
 Cl[7] = ['Italy', 0, 32123, 0.7]
 Cl[8] = ['Luxembourg', 0, 532887, 0.4]
 Cl[9] = ['Malta', 0, 65474, 0.8]
-Cl[10] = ['Norway', 0, 643864, 0.95]
-
-
-'''
-plots graph
-'''
+Cl[10] = ['Norway', 0, 643864, 0.65]
+Cl[11] = ['Spain', 0, 57276, 0.3]
+Cl[12] = ['Switzerland', 0, 9876, 0.44]
+Cl[13] = ['Netherland', 0, 85885, 0.52]
 
 
 G = nx.Graph()
-cities = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+cities = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 #paths is the list of edges/edge connections as first two elements in each tuple of the list and the weight (in decimals) of the edge as third element
-paths = [(1, 3, 0.3), (7, 8, 0.2), (9, 10, 0.2), (10, 1, 0.1), (10, 6, 0.4), (10, 8, 0.2), (10, 5, 0.5), (2, 3, 0.5), (8, 0, 0.4), (9, 1, 0.3), (9, 5, 0.6), (9, 2, 0.7), (4, 1, 0.7), (1, 2, 0.6), (8, 3, 0.5), (8, 6, 0.3), (8, 1, 0.45), (6, 1, 0.5), (3, 6, 0.4), (7, 1, 0.2), (6, 7, 0.3), (0, 5, 0.4), (0, 1, 0.45), (2, 4, 0.2), (3, 5, 0.37), (4, 5, 0.55)]
+paths = [(1, 3, 0.4), (7, 8, 0.3), (13, 1, 0.2), (13, 4, 0.7), (13, 0, 0.4), (13, 9, 0.5), (13, 12, 0.6), (13, 4, 0.55), (11, 10, 0.6), (12, 11, 0.5), (12, 3, 0.5), (12, 5, 0.2), (12, 8, 0.3), (6, 11, 0.7), (11, 2, 0.5), (11, 8, 0.8), (9, 10, 0.56), (10, 1, 0.5), (10, 6, 0.4), (10, 8, 0.6), (10, 5, 0.5), (2, 3, 0.5), (8, 0, 0.7), (9, 1, 0.3), (9, 5, 0.6), (9, 2, 0.7), (4, 1, 0.7), (1, 2, 0.6), (8, 3, 0.5), (8, 6, 0.3), (8, 1, 0.45), (6, 1, 0.5), (3, 6, 0.4), (7, 1, 0.25), (6, 7, 0.3), (0, 5, 0.4), (0, 1, 0.45), (2, 4, 0.62), (3, 5, 0.47), (4, 5, 0.55)]
 G.add_nodes_from(cities)
 G.add_weighted_edges_from(paths)
 pos = nx.circular_layout(G) #form of layout of graph I want. Since I want a Hamiltonian cycle, then I want something circular for easy interpretation
@@ -73,14 +71,15 @@ def intera(i, nodes, ct, route):
                     
                         
 def hamiltonian(G, cities):#Hamiltonian cycle
-    global dic, nodes, ct, route, s_vertex, d_places
+    global dic, nodes, ct, route, s_vertex, d_places, Continue
     dic = {}
     d_places = cities[:]
     try:
         s_vertex = 0 #relief center
     except KeyError:
         Continue = False
-        return("Used all Hamiltonian Cycles possible")
+        print ("\nUsed all Hamiltonian Cycles possible \n")
+        return
     for i in cities:
         if len(G.neighbors(i)) <=1:
             if i != s_vertex:
@@ -97,15 +96,16 @@ def hamiltonian(G, cities):#Hamiltonian cycle
         start = dic[s_vertex] #starting vertex/residence
     except KeyError:
         Continue = False
-        return("Used all Hamiltonian Cycles possible")
+        print ("\nUsed all Hamiltonian Cycles possible \n")
+        return 
  
     route = []
  
-    for i in start:
+    for m in start:
         nodes = cities[:]
-        ct = [s_vertex, i]
-        nodes.remove(i)
-        intera(i, nodes, ct, route)
+        ct = [s_vertex, m]
+        nodes.remove(m)
+        intera(m, nodes, ct, route)
                    
     return route
 
@@ -114,21 +114,25 @@ def flow(G, cities):
     Circuits =  hamiltonian(G, cities)
     H_Cycle = []
     Sorted = []
-
-    try:
-        for g in Circuits:
-            weight_sum = 0
-            num = len(g) - 1
-            while num > 0:
-                weight_sum += G[g[num]][g[num - 1]]['weight']
-                num -= 1
-            g.append(weight_sum)
-    except AttributeError:
-        Continue = False
-        return("Used all Hamiltonian Cycles possible")
+    if Continue:
+        try:
+            for g in Circuits:
+                weight_sum = 0
+                num = len(g) - 1
+                while num > 0:
+                    weight_sum += G[g[num]][g[num - 1]]['weight']
+                    num -= 1
+                g.append(weight_sum)
+        except AttributeError:
+            Continue = False
+            print ("\nUsed all Hamiltonian Cycles possible \n")
+            return
+    else:
+        return
+        
     #print ("This is the list of all Hamiltonian cycles and their weights at the end: \n", Circuits)
     #print("")
-    print("There are", len(Circuits), "number of Hamiltonian circuits possible.")
+    print("\nThere are", len(Circuits), "number of Hamiltonian circuits possible.")
     print("")
 
     for i in Circuits:
@@ -139,7 +143,7 @@ def flow(G, cities):
             H_Cycle.append(i)
     #print("The most optimal Hamiltonian cycle here are: \n", H_Cycle)
     #print("")
-    print("There are", len(H_Cycle), "number of Hamiltonian circuits without repetition")
+    print("\nThere are", len(H_Cycle), "number of Hamiltonian circuits without repetition")
     print("")
 
     if len(H_Cycle) != 0:
@@ -155,7 +159,7 @@ def flow(G, cities):
         print("")
     else:
         Continue = False
-        print("No Hamiltonian circuit.")
+        print("No Hamiltonian circuit. \n")
         
     if Continue:
         y = len(opt) - 1
@@ -186,16 +190,18 @@ def flow(G, cities):
             
 def disease(y, N):
     J = round(0.0001*(y)*(N-y), 0)
-    return J
+    return int(J)
+
 Continue = True
 
 d_places = cities[:]
 def main(G, cities, Cl, d_places):
     a = 0
     places = cities[:]
+    
     while Continue:
         a += 1
-        flow(G, places)
+        flow(G, places) #changed this from 'places' to 'rem'
         if Continue:
             for g in d_places:
                 if Cl[g][1] > Cl[g][2]:
@@ -215,12 +221,14 @@ def main(G, cities, Cl, d_places):
                                 if z <= ((Cl[i][1]/Cl[i][2]) * (1 - G[i][m]['weight'])):
                                     t = z * Cl[i][2]/(1 -G[i][m]['weight'])
                                     if (t != 0) and (t == round(t, 0)):
-                                        k = t
+                                        k = int(t)
                                     else:
                                         k = int(t) + 1
                                     Cl[m][1] += k
                                     if Cl[m][1] > Cl[m][2]:
                                         Cl[m][1] = Cl[m][2]
+            for v in d_places: #people cured from medication
+                Cl[v][1] -= round(0.1*Cl[v][1])               
             
         for y in range(len(Cl)):
             if y == len(Cl) - 1:
@@ -228,11 +236,29 @@ def main(G, cities, Cl, d_places):
             else:
                 print(Cl[y], end = ', ')
         print("")
+
+        isolated = True
+        rem = [] #remaining nodes in circuit
+        for u, l in G.edges():
+            if u not in rem:
+                rem.append(u)
+            if l not in rem:
+                rem.append(l)
+        if len(rem) != 0:
+            for r in rem:
+                if Cl[r][1] > 0:
+                        isolated = False
+        if isolated and (len(rem) != 0):
+            print("\nThere was successful isolation. Diesease can no longer spread in network!")
+            a -= 1 #successful isolation occured during the last step
+            break
+        
         for f in cities:
             for j in G.neighbors(f):
                 if Cl[j][1]/Cl[j][2] > Cl[f][3]:
                     print ("Ban transportation between", Cl[f][0], "and",Cl[j][0])
-                    G.remove_edge(f, j) #bans flight between two countries
+                    G.remove_edge(f, j)
+        
     print("")               
     print("Could only travel about", a, "times.")
     new_lab = {}
